26c509f8e69030c26ebd9387519b1c1ff2b3afb7
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-08-30 02:34:07 +0530

    added app.route()

--- app.js
+++ app.js
@@ -2,57 +2,59 @@
 
 const express = require("express");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/wikiDB');
+mongoose.connect("mongodb://localhost:27017/wikiDB");
 
 const articleSchema = {
     title: String,
-    content: String
+    content: String,
 };
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", (req, res) => {
-    Article.find({}).then((result) => {
-        console.log(result);
-    }).catch((err) => {
-        console.log(err);
-    });
-})
+app.route("/articles")
+    .get((req, res) => {
+        Article.find({})
+            .then((result) => {
+                res.send(result);
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    })
+    .post((req, res) => {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content,
+        });
 
-app.post("/articles", (req, res) => {
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        newArticle
+            .save()
+            .then(() => {
+                res.send("Successfully saved article");
+            })
+            .catch((err) => {
+                res.send(err);
+            });
+    })
+    .delete((req, res) => {
+        Article.deleteMany({})
+            .then(() => {
+                res.send("Successfully deleted all articles");
+            })
+            .catch((err) => {
+                res.send(err);
+            });
     });
 
-    newArticle.save().then(() => {
-        console.log("Successfully saved article");
-        res.send("Successfully saved article");
-    }).catch((err) => {
-        console.log(err);
-        res.send(err);
-    });
-});
-
-app.delete("/articles", (req, res) => {
-    Article.deleteMany({}).then(() => {
-        console.log("Successfully deleted all articles");
-        res.send("Successfully deleted all articles");
-    }).catch((err) => {
-        console.log(err);
-        res.send(err);
-    });
-});
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
