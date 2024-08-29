49c6839885d67bea7629a8752bcca440ea8a6102
dpiyumal2319 <132676592+dpiyumal2319@users.noreply.github.com>
2024-08-29 22:57:29 +0530

    Added mongoose starting code

--- app.js
+++ app.js
@@ -11,7 +11,14 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//TODO
+mongoose.connect('mongodb://localhost:27017/wikiDB');
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
